a6c6f26f0982cd982c1885e3fb824d649d49247b
Jonas Andreasson <jo7334an-s@beta-5.e.lth.lu.se>
2023-01-26 16:02:54 +0100

    Linked list done

--- lab1/list.cc
+++ lab1/list.cc
@@ -1,30 +1,102 @@
 #include <iostream>
+#include <string>
 #include "list.h"
+using std::cout;
+
 
 List::List() {
+	first = nullptr;	
 }
 
 List::~List() {
 }
 
 bool List::exists(int d) const {
-	return true;
+	Node* temp = first;
+	while(temp != nullptr){
+		if (temp -> value == d){
+			return true;
+		}
+		temp = temp->next;
+	}
+	return false;
 }
 
 int List::size() const {
-	return 0;
+	int counter(0);
+	Node* temp = first;
+	while(temp != nullptr){
+		counter++;
+		temp = temp->next;
+	}
+	return counter;
 }
 
 bool List::empty() const {
-	return true;
+	return first==nullptr;
 }
 
 void List::insertFirst(int d) {
+	Node* temp = new Node(d, first);
+	first = temp;
 }
 
 void List::remove(int d, DeleteFlag df) {
+	if (empty()){
+		return;
+	}
+	Node* temp = first;
+	Node* previous = first;
+	while(temp != nullptr){
+		if (df == List::DeleteFlag::EQUAL){
+			if ((temp -> value) == d){
+				if(previous == temp){
+					first = temp->next;
+					return;
+				}
+				previous->next = temp->next;
+				return;
+			}
+		}
+		if (df == List::DeleteFlag::LESS){
+			if ((temp -> value) < d){
+				if(previous == temp){
+					first = temp->next;
+					return;
+				}
+				previous->next = temp->next;
+				return;
+			}
+		}
+		if (df == List::DeleteFlag::GREATER){
+			if ((temp -> value) > d){
+				if(previous == temp){
+					first = temp->next;
+					return;
+				}
+				previous->next = temp->next;
+				return;
+			}
+			
+		}
+		previous = temp;
+		temp = temp->next;
+	}
 }
 
 void List::print() const {
-}
+	Node* temp = first;
+	int counter = 0;
+	cout << "Printing List: \n";
+	if (empty()){
+		cout << "Empty!" << std::endl;
+		return;
+	}
+	while(temp != nullptr){
+		cout << "Node " << counter << "="<<(temp->value) << "\n";
+		counter++;
+		temp = temp->next;
+	}
+	cout << std::endl;
 
+}
