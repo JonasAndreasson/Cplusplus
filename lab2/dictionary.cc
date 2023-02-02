91c4547abf7e5eba14bc21ea00a8ece93d19412e
Jonas Andreasson <jo7334an-s@gamma-4.e.lth.lu.se>
2023-02-02 16:28:48 +0100

    part of dictionary done

--- lab2/dictionary.cc
+++ lab2/dictionary.cc
@@ -3,13 +3,28 @@
 #include <fstream>
 #include <iostream>
 #include <algorithm>
+#include <unordered_set>
 #include "word.h"
 #include "dictionary.h"
 
 using std::string;
 using std::vector;
+using std::unordered_set;
+using std::ifstream;
 
 Dictionary::Dictionary() {
+	ifstream in_file;
+	string w;
+	string delim = "";
+	unordered_set<Word> dict;
+	in_file.open("words.txt");
+	if (in_file.is_open()){
+		while (getline(in_file, w, '\n'))
+			{
+				string s = w.substr(0, w.find(delim));
+				Word word_obj(s, {})
+			}
+	}
 }
 
 bool Dictionary::contains(const string& word) const {
