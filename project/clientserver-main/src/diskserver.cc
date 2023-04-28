7c7424d634c4e502b9ac4d5bea826b203b60e3e3
Jonas Andreasson <jo7334an-s@student.lu.se>
2023-04-28 11:12:06 +0200

    Diskserver completed
    Done with Diskserver, could use some cleaning up!

--- project/clientserver-main/src/diskserver.cc
+++ project/clientserver-main/src/diskserver.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <dirent.h>
@@ -29,22 +30,115 @@ size_t DiskServer::database_size(){
 
     dirp = opendir("./database"); /* There should be error handling after this */
     while ((entry = readdir(dirp)) != NULL) {
-        if (entry->d_type == DT_DIR) { /* If the entry is a regular file */
+        if (entry->d_type == DT_DIR) {
+            std::string subpath = entry->d_name; //converting for easier comparisons.
+            if (subpath == "." || subpath==".."){
+
+            } else {
+            cout << entry->d_name << endl;
             file_count++; 
+            }
         }
     }
     closedir(dirp);
-    return file_count-2;
+    return file_count;
+}
+size_t DiskServer::newsgroup_size(std::string path){
+    int file_count = 0;
+    DIR * dirp;
+    struct dirent * entry;
+
+    dirp = opendir(path.c_str()); /* There should be error handling after this */
+    while ((entry = readdir(dirp)) != NULL) {
+        if (entry->d_type == DT_DIR) {
+            std::string subpath = entry->d_name; //converting for easier comparisons.
+            if (subpath == "." || subpath==".."){
+
+            } else {
+            cout << entry->d_name << endl;
+            file_count++; 
+            }
+        }
+    }
+    closedir(dirp);
+    return file_count;
 }
 
 
-int DiskServer::get_line_newsgroup(uint32_t id){}
-int DiskServer::get_line_article(uint32_t id){}
-void DiskServer::delete_article(int line){}
-void DiskServer::add_article(Article art){}
-void DiskServer::add_newsgroup(Newsgroup ng){}
-void DiskServer::delete_newsgroup(int line){}
-std::vector<ServerInterface::Article> DiskServer::articles_related_to_newsgroup(int line){}
+void DiskServer::add_article(Article art, std::string path){
+    path += std::to_string(art.id);
+    auto ng_index = mkdir(path.c_str(),S_IRWXU);
+    std::ofstream file_stream;
+    file_stream.open(path+"/title");
+    file_stream << art.title;
+    file_stream.close();
+
+    file_stream.open(path+"/author");
+    file_stream << art.author;
+    file_stream.close();
+
+    file_stream.open(path+"/text");
+    file_stream << art.text;
+    file_stream.close();
+    file_stream.open(path+"/created");
+    file_stream << art.created;
+    file_stream.close();
+    file_stream.open(path+"/id");
+    file_stream << art.id;
+    file_stream.close();
+}
+std::vector<ServerInterface::Article> DiskServer::articles_related_to_newsgroup(std::string path){
+    std::vector<ServerInterface::Article> list;
+    std::string line;
+    std::ifstream file;
+    DIR * dirp, *sbdir;
+    struct dirent * entry;
+    dirp = opendir(path.c_str());
+    while ((entry = readdir(dirp)) != NULL) {
+    if (entry->d_type == DT_DIR) {
+        std::string subpath = entry->d_name;
+        if (subpath == "." || subpath == ".."){
+
+        } else {
+        subpath = path +"/"+ subpath;
+        std::string title;
+        std::string buffer;
+        file.open(subpath+"/title");
+        while (file >> buffer){
+            title += buffer + " ";
+        }
+        title = title.substr(0,title.size()-1);
+        file.close();
+        std::string author;
+        file.open(subpath+"/author");
+        while (file >> buffer){
+            author += buffer + " ";
+        }
+        author = author.substr(0,author.size()-1);
+        file.close();
+        std::string text;
+        file.open(subpath+"/text");
+        while (file >> buffer){
+            text += buffer + " ";
+        }
+        text = text.substr(0,text.size()-1);
+        file.close();
+        uint32_t id;
+        file.open(subpath+"/id");
+        file >> id;
+        file.close();
+        time_t created;
+        file.open(subpath+"/created");
+        file >> created;
+        file.close();
+        Article art = {title,author,text,id,created};
+        list.push_back(art);
+        }
+    }
+    }
+    closedir(dirp);
+    return list;
+}
 std::vector<ServerInterface::Newsgroup> DiskServer::newsgroup_list(){
     std::vector<ServerInterface::Newsgroup> list;
     std::string line;
@@ -81,9 +175,33 @@ std::vector<ServerInterface::Newsgroup> DiskServer::newsgroup_list(){
     closedir(dirp);
     return list;
 }
-std::string DiskServer::get_article_title(int line){}
-std::string DiskServer::get_article_author(int line){}
-std::string DiskServer::get_article_text(int line){}
+std::string DiskServer::get_article_title(std::string path){
+    std::ifstream f(path+"/title");
+    std::stringstream s;
+    if(f){
+        s << f.rdbuf();
+        f.close();
+    }
+    return s.str();
+}
+std::string DiskServer::get_article_author(std::string path){
+    std::ifstream f(path+"/author");
+    std::stringstream s;
+    if(f){
+        s << f.rdbuf();
+        f.close();
+    }
+    return s.str();
+}
+std::string DiskServer::get_article_text(std::string path){
+    std::ifstream f(path+"/text");
+    std::stringstream s;
+    if(f){
+        s << f.rdbuf();
+        f.close();
+    }
+    return s.str();
+}
 
 
 DiskServer::DiskServer(int port):server(port){
@@ -172,7 +290,7 @@ void rm_subdir(std::string path){
     closedir(dirp);
     rmdir(path.c_str());
 }
-void DiskServer::remove_newsgroup(std::shared_ptr<Connection>& conn){
+void DiskServer::remove_newsgroup(std::shared_ptr<Connection>& conn){//スーパー　ありがとうございます！今は3時三十四分です。
     if ((Protocol)conn->read() != Protocol::PAR_NUM){
         return;
     }
@@ -202,21 +320,23 @@ void DiskServer::list_article(std::shared_ptr<Connection>& conn){
     if ((Protocol)conn->read() != Protocol::COM_END){
         return;
     }
-    auto ng_index = get_line_newsgroup(id);
-    bool exists = ng_index != -1;
+    std::string path = "database/"+std::to_string(id);
+    auto ng_index = mkdir(path.c_str(),S_IRWXU);
+    bool exists = ng_index == -1;
     conn->write((unsigned char)Protocol::ANS_LIST_ART);
 
     if (exists){
         conn->write((unsigned char)Protocol::ANS_ACK);
         conn->write((unsigned char)Protocol::PAR_NUM);
-        unsigned int size = database_size();
+        unsigned int size = newsgroup_size(path);
         send_N(conn,size);
-        for (Article& a : articles_related_to_newsgroup(ng_index)){
+        for (Article& a : articles_related_to_newsgroup(path)){
             conn->write((unsigned char)Protocol::PAR_NUM);
             send_N(conn,a.id);
             send_string_p(conn, a.title);
         }
     } else{
+        rmdir(path.c_str());
         conn->write((unsigned char)Protocol::ANS_NAK);
         conn->write((unsigned char)Protocol::ERR_NG_DOES_NOT_EXIST);
     }
@@ -257,14 +377,16 @@ void DiskServer::create_article(std::shared_ptr<Connection>& conn){
         return;
     }
 
-    auto ng_index = get_line_newsgroup(news_group_id);
-    bool exists = ng_index != -1;
+    std::string path = "database/"+std::to_string(news_group_id);
+    auto ng_index = mkdir(path.c_str(),S_IRWXU);
+    bool exists = ng_index == -1;
     conn->write((unsigned char) Protocol::ANS_CREATE_ART);
     if (exists){
         conn->write((unsigned char) Protocol::ANS_ACK);
         Article art{title, author, text, (uint32_t)std::hash<std::string>{}(text),time(NULL)}; //Forcing 32bits conversion to accomadate for 32bit architecture.
-        add_article(art);
+        add_article(art, path+"/");
     } else {
+        rmdir(path.c_str());
         conn->write((unsigned char) Protocol::ANS_NAK);
         conn->write((unsigned char) Protocol::ERR_NG_DOES_NOT_EXIST);
     }
@@ -282,19 +404,23 @@ void DiskServer::delete_article(std::shared_ptr<Connection>& conn){
     if((Protocol)conn->read()!=Protocol::COM_END){
         return;
     }
-    auto ng_index = get_line_newsgroup(news_group_id);
-    bool exists = ng_index != -1;
+    std::string path = "database/"+std::to_string(news_group_id);
+    auto ng_index = mkdir(path.c_str(),S_IRWXU);
+    bool exists = ng_index == -1;
     conn->write((unsigned char) Protocol::ANS_DELETE_ART);
     if (exists){
-        auto art_index = get_line_article(article_id);
-        if(art_index!=-1){
-            delete_article(art_index);
+        path+="/"+std::to_string(article_id);
+        auto art_index = mkdir(path.c_str(),S_IRWXU);
+        if(art_index==-1){
+            rm_subdir(path);
             conn->write((unsigned char) Protocol::ANS_ACK);    
         } else {
+        rmdir(path.c_str());
         conn->write((unsigned char) Protocol::ANS_NAK);
         conn->write((unsigned char) Protocol::ERR_ART_DOES_NOT_EXIST);
         }
     } else {
+        rmdir(path.c_str());
         conn->write((unsigned char) Protocol::ANS_NAK);
         conn->write((unsigned char) Protocol::ERR_NG_DOES_NOT_EXIST);
     }
@@ -312,27 +438,31 @@ void DiskServer::get_article(std::shared_ptr<Connection>& conn){
     if((Protocol)conn->read()!=Protocol::COM_END){
         return;
     }
-    auto ng_index = get_line_newsgroup(news_group_id);
-    bool exists = ng_index != -1;
+    std::string path = "database/"+std::to_string(news_group_id);
+    auto ng_index = mkdir(path.c_str(),S_IRWXU);
+    bool exists = ng_index == -1;
     conn->write((unsigned char) Protocol::ANS_GET_ART);
     if (exists){
-        auto art_index = get_line_article(article_id);
-        if(art_index!=-1){
+        path += "/"+std::to_string(article_id);
+        auto art_index = mkdir(path.c_str(),S_IRWXU);
+        if(art_index==-1){
             conn->write((unsigned char) Protocol::ANS_ACK);
             //titel
-            std::string title = get_article_title(art_index);
+            std::string title = get_article_title(path);
             send_string_p(conn, title);
             //author
-            std::string author = get_article_author(art_index);
+            std::string author = get_article_author(path);
             send_string_p(conn, author);
             //text
-            std::string text = get_article_text(art_index);
+            std::string text = get_article_text(path);
             send_string_p(conn, text);
         } else {
+        rmdir(path.c_str());
         conn->write((unsigned char) Protocol::ANS_NAK);
         conn->write((unsigned char) Protocol::ERR_ART_DOES_NOT_EXIST);
         }
     } else {
+        rmdir(path.c_str());
         conn->write((unsigned char) Protocol::ANS_NAK);
         conn->write((unsigned char) Protocol::ERR_NG_DOES_NOT_EXIST);
     }
--- project/clientserver-main/src/diskserver.h
+++ project/clientserver-main/src/diskserver.h
@@ -8,18 +8,14 @@ class DiskServer : public ServerInterface{
     public:
     DiskServer(int port);
     size_t database_size();
-    int get_line_newsgroup(uint32_t id);
-    int get_line_article(uint32_t id);
-    void delete_article(int);
-    void add_article(Article);
-    void add_newsgroup(Newsgroup);
-    void delete_newsgroup(int);
+    size_t newsgroup_size(std::string);
+    void add_article(Article, std::string);
     Newsgroup parse_newsgroup(std::string);
-    std::vector<Article> articles_related_to_newsgroup(int);
+    std::vector<Article> articles_related_to_newsgroup(std::string);
     std::vector<Newsgroup> newsgroup_list();
-    std::string get_article_title(int);
-    std::string get_article_author(int);
-    std::string get_article_text(int);
+    std::string get_article_title(std::string);
+    std::string get_article_author(std::string);
+    std::string get_article_text(std::string);
     virtual bool isReady();
     virtual void serve_one();
     virtual void list_newsgroup(std::shared_ptr<Connection>& conn);
